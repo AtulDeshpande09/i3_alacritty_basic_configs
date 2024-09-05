c224cffa09c189ade3f5a0f75b904fb571941c79
Atul Deshpande <159980686+AtulDeshpande09@users.noreply.github.com>
2024-09-05 19:49:17 +0530

    Create config.py

---
+++ qutebrowser/config.py
@@ -0,0 +1,20 @@
+#start page
+
+c.url.star_pages = 'https://duckduckgo.com'
+
+# status bar
+c.statusbar.show = 'never'
+
+# appearance
+
+#tabs background
+c.colors.tabs.odd.bg = '#fa00d6'
+c.colors.tabs.even.bg = '#b558ca'
+c.colors.tabs.selected.odd.bg = '#0051fa'
+c.colors.tabs.selected.even.bg = '#ffc008'
+
+# text
+c.colors.tabs.odd.fg = '#ffffff'
+c.colors.tabs.even.fg = '#ffffff'
+c.colors.tabs.selected.odd.fg = '#ffffff'
+c.colors.tabs.selected.even.fg = '#ffffff'
